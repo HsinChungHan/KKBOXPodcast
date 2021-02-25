67b066be2bd3009bd23ca92de7c8da4c4cc5d473
HsinChungHan <hooy123456@gapp.nthu.edu.tw>
2021-02-25 15:16:03 +0800

    add objc bridging header

---
+++ KKBOXPodcast/Models/ObjcEpisode.h
@@ -0,0 +1,33 @@
+//
+//  ObjcEpisode.h
+//  KKBOXPodcast
+//
+//  Created by Chung Han Hsin on 2021/2/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ObjcEpisode : NSObject
+
+@property (nonatomic, strong) NSString* title;
+@property (nonatomic, strong) NSDate* pubDate;
+@property (nonatomic, strong) NSString* subtitle;
+@property (nonatomic, strong) NSString* author;
+@property (nonatomic, strong) NSString* streamUrl;
+@property (nonatomic, strong) NSString* summary;
+@property (nonatomic, strong) NSString* imageUrl;
+@property (nonatomic, strong) NSString* fileUrl;
+
+- (id)initWithTitle:(NSString*)title
+             pubDate:(NSDate*)pubDate
+            subtitle:(NSString*)subtitle
+              author:(NSString*)author
+           streamUrl:(NSString*)streamUrl
+             summary:(NSString*)summary
+            imageUrl:(NSString*)imageUrl
+            fileUrl:(NSString*)fileUrl;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KKBOXPodcast/Models/ObjcEpisode.m
@@ -0,0 +1,36 @@
+//
+//  ObjcEpisode.m
+//  KKBOXPodcast
+//
+//  Created by Chung Han Hsin on 2021/2/25.
+//
+
+#import "ObjcEpisode.h"
+#import <FeedKit/FeedKit-Swift.h>
+
+
+@implementation ObjcEpisode
+
+
+-(id)initWithTitle:(NSString*)title
+           pubDate:(NSDate*)pubDate
+          subtitle:(NSString*)subtitle
+            author:(NSString*)author
+         streamUrl:(NSString*)streamUrl
+           summary:(NSString*)summary
+          imageUrl:(NSString*)imageUrl
+           fileUrl:(NSString*)fileUrl
+{
+    if (!(self = [super init]))
+        return nil;
+    self.title = title;
+    self.pubDate = pubDate;
+    self.subtitle = subtitle;
+    self.author = author;
+    self.streamUrl = streamUrl;
+    self.imageUrl = imageUrl;
+    self.fileUrl = fileUrl;
+    return self;
+}
+
+@end
